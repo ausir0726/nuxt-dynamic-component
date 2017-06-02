2b2bdb5318ee0d973be8da286e3a5f83fd8300ae
ausir <ausir@finpo.com.tw>
2017-06-03 00:07:18 +0800

    use component on header , and plugin ssr false

---
+++ components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  <h1>header</h1>
+  <editor flag="news-pub" v-model="newcon">
+  </editor>
+</div>
+</template>
+<script>
+
+export default {
+  data () {
+    return { newcon: '' }
+  }
+}
+</script>
