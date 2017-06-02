382c5a9b1b29c17112418701b4416b34b3a214c5
ausir <ausir@finpo.com.tw>
2017-06-03 00:54:07 +0800

    add loading and interval test

--- components/Header.vue
+++ components/Header.vue
@@ -1,8 +1,9 @@
 <template>
 <div>
-  <h1>header</h1>
-  <editor flag="news-pub" v-model="newcon">
+  <h1>header - counter {{counter}}</h1>
+  <editor v-if="!editorLoading" flag="news-pub" v-model="newcon">
   </editor>
+  <div v-else>editor loading.. please wait</div>
   <pre>{{newcon}}</pre>
 </div>
 </template>
@@ -11,12 +12,17 @@ import Vue from 'vue'
 
 export default {
   data () {
-    return { newcon: '' }
+    return { newcon: '', editorLoading: true, counter: 0, interval: 0 }
+  },
+  beforeMount () {
+    this.interval = setInterval(() => { this.counter += 1 }, 500)
   },
   async mounted () {
     const editor = await import('~components/editor.vue')
     Vue.component('editor', editor)
-    this.$forceUpdate()
+    // this.$forceUpdate()
+    this.editorLoading = false // this will trigger component rerender
+    clearInterval(this.interval)
   }
 }
 </script>
